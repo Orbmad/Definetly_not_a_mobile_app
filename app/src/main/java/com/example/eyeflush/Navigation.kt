b27a10dc9d428b4ae5f69a7a116fbb38693ffe70
Michele Farneti <michele.farneti@studio.unibo.it>
2025-05-22 17:21:42 +0200

    MainActivity basic setup

---
+++ app/src/main/java/com/example/eyeflush/Navigation.kt
@@ -0,0 +1,8 @@
+package com.example.eyeflush
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun EyeFlushNavGraph(navController: NavHostController) {
+}
