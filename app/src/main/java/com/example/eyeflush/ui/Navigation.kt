51c4675ec684e269c0676e72f19a94c751d280ea
Michele Farneti <michele.farneti@studio.unibo.it>
2025-05-26 00:10:04 +0200

    Added simple but working splashScreen

--- app/src/main/java/com/example/eyeflush/ui/Navigation.kt
+++ app/src/main/java/com/example/eyeflush/ui/Navigation.kt
@@ -1,12 +1,15 @@
-package com.example.eyeflush.ui.theme
+package com.example.eyeflush.ui
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.eyeflush.ui.screens.splashscreen.SplashScreen
 import kotlinx.serialization.Serializable
-import org.koin.androidx.compose.koinViewModel
 
 sealed interface EyeFlushRoute {
-    @Serializable data object SplashScreen : EyeFlushRoute
+    //Navigation with kotlin serialization
+    @Serializable data object Splash : EyeFlushRoute
     @Serializable data object SignIn : EyeFlushRoute
     @Serializable data object SignUp : EyeFlushRoute
     @Serializable data object Home : EyeFlushRoute
@@ -14,5 +17,13 @@ sealed interface EyeFlushRoute {
 
 @Composable
 fun EyeFlushNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = EyeFlushRoute.Splash //Default destination
+    ){
+        composable<EyeFlushRoute.Splash> {
+            SplashScreen(navController)
+        }
+    }
 
 }
--- app/src/main/java/com/example/eyeflush/ui/screens/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/eyeflush/ui/screens/splashscreen/SplashScreen.kt
@@ -1,8 +1,21 @@
 package com.example.eyeflush.ui.screens.splashscreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @Composable
-fun SplashScreen( ){
-
+fun SplashScreen(navController: NavHostController) {
+    // Grafica della splash screen
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("EyeFlush", style = MaterialTheme.typography.headlineMedium)
+    }
 }
