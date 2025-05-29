64fab0278d4f140448307303f17a1d72bbfdeb72
Michele Farneti <michele.farneti@studio.unibo.it>
2025-05-29 14:45:26 +0200

    Added file for icons paths

---
+++ app/src/main/java/com/dambrofarne/eyeflush/data/constants/IconPaths.kt
@@ -0,0 +1,9 @@
+package com.dambrofarne.eyeflush.data.constants
+
+import androidx.annotation.DrawableRes
+import com.dambrofarne.eyeflush.R
+
+object IconPaths {
+    //@DrawableRes
+    //val ICON_GALLERY = R.drawable.ic_home
+}
