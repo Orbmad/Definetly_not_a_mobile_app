fd8a0522bbad478ceb905363d1c5c475a25b9103
Manuele D'Ambrosio <dambrom01@gmail.com>
2025-06-12 11:47:10 +0200

    Small changes, unstable state

--- app/src/main/java/com/dambrofarne/eyeflush/ui/screens/home/HomeMapViewModel.kt
+++ app/src/main/java/com/dambrofarne/eyeflush/ui/screens/home/HomeMapViewModel.kt
@@ -1,11 +1,13 @@
 package com.dambrofarne.eyeflush.ui.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dambrofarne.eyeflush.data.repositories.auth.AuthRepository
 import com.dambrofarne.eyeflush.data.repositories.database.DatabaseRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import org.osmdroid.util.GeoPoint
 
 class HomeMapViewModel(
@@ -22,5 +24,16 @@ class HomeMapViewModel(
         _currentLocation.value = location
     }
 
+//    fun addPhotoMarker(photoID: String, photoUri: String, location: GeoPoint) {
+//        viewModelScope.launch {
+//            val newMarker = PhotoMarker(
+//                id = photoID,
+//                position = location,
+//                photoUri = photoUri
+//            )
+//            _photoMarkers.value += newMarker
+//        }
+//    }
 
+    
 }
