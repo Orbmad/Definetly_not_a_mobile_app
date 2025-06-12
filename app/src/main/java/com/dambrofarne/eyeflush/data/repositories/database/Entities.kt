ac2122cb8c7fe1e5c0bb873109231e5e58523fe3
Michele Farneti <michele.farneti@studio.unibo.it>
2025-06-12 17:08:01 +0200

    Added images count parameter to markers

--- app/src/main/java/com/dambrofarne/eyeflush/data/repositories/database/Entities.kt
+++ app/src/main/java/com/dambrofarne/eyeflush/data/repositories/database/Entities.kt
@@ -8,7 +8,8 @@ data class Marker(
     val name: String?,
     val coordinates: GeoPoint,
     val mostLikedPicId : String?,
-    val mostLikedPicURL: String?
+    val mostLikedPicURL: String?,
+    val imagesCount : Int
 )
 
 data class MarkerRaw(
@@ -17,13 +18,15 @@ data class MarkerRaw(
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
     val mostLikedPicId: String? = "",
-    val mostLikedPicURL: String? = ""
+    val mostLikedPicURL: String? = "",
+    val imagesCount : Int = 0
 ) {
     fun toMarker() = Marker(
         id = id,
         name = name,
         coordinates = GeoPoint(latitude, longitude),
         mostLikedPicId = mostLikedPicId,
-        mostLikedPicURL = mostLikedPicURL
+        mostLikedPicURL = mostLikedPicURL,
+        imagesCount = imagesCount
     )
 }
