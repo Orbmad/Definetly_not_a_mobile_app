6553d8e51b96082b96bc03db710f49a323220b94
Michele Farneti <michele.farneti@studio.unibo.it>
2025-06-19 13:33:35 +0200

    Small changes to profile config

--- app/src/main/java/com/dambrofarne/eyeflush/ui/screens/profileconfig/ProfileConfigScreen.kt
+++ app/src/main/java/com/dambrofarne/eyeflush/ui/screens/profileconfig/ProfileConfigScreen.kt
@@ -12,7 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
